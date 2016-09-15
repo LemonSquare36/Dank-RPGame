7d7a49c9c447881fd82f83860a23f3c4fd651453
jglover <jglover@PRG23656>
2016-09-15 10:38:08 -0400

    Still needs work DONT TOUCH

--- Game1/Game1/Polygons.cs
+++ Game1/Game1/Polygons.cs
@@ -15,5 +15,26 @@ namespace Main
 {
     class Polygons
     {
+        public Triangles Triangle;
+        public Triangles Triangle2;
+
+        public void ListAdd()
+        {
+            // When Adding Verticies make the First Vector2 in the list the Center point of the shape!!!!
+            Triangle.Verticies.Add(new Vector2(119, 160));//Marking the Center with comment lines
+            Triangle.Verticies.Add(new Vector2(112, 0));
+            Triangle.Verticies.Add(new Vector2(0, 232));
+            Triangle.Verticies.Add(new Vector2(224, 232));
+
+            Triangle2.Verticies.Add(new Vector2(119, 160));//
+            Triangle2.Verticies.Add(new Vector2(112, 0));
+            Triangle2.Verticies.Add(new Vector2(0, 232));
+            Triangle2.Verticies.Add(new Vector2(224, 232));
+
+            Debug.WriteLine(Triangle.Verticies[0]);
+            Debug.WriteLine(Triangle.Verticies[1]);
+            Debug.WriteLine(Triangle.Verticies[2]);
+
+        }
     }
 }
--- Game1/Game1/Triangles.cs
+++ Game1/Game1/Triangles.cs
@@ -14,7 +14,32 @@ using System.Diagnostics;
 
 namespace Main
 {
-    class Triangles
+    class Triangles : Polygons
     {
+        Texture2D triangle, Trapazoid;
+        public enum Shape { Triangle }
+        Shape Ptype;
+        float rotation = 2;
+        SpriteEffects effects;
+        public Vector2 Placement { get; set; }
+
+
+        public List<Vector2> Verticies = new List<Vector2>();
+
+        public void LoadContent(ContentManager theContentManager)
+        {
+            triangle = Main.GameContent.Load<Texture2D>("Sprites/GreenTriangle");
+        }
+
+        public void update(GameTime gameTime)
+        {
+            //rotation = rotation + .04f;
+            Placement = new Vector2(Placement.X + 0.5f, Placement.Y);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(triangle, Placement, null, null, Verticies[0], rotation, null, null);
+        }
     }
 }
