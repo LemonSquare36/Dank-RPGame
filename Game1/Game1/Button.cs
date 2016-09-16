ab6640de322b9681902db9b42c4e95c4981e8b40
mamaher <mamaher@PRG23652>
2016-09-16 09:16:35 -0400

    Menu work

--- Game1/Game1/Button.cs
+++ Game1/Game1/Button.cs
@@ -16,5 +16,53 @@ namespace Game1
 {
     class Button
     {
+        private enum ButtonType { Rectangle };
+        private float diameter { get; set; }
+        private float windowWidth { get; set; }
+        private ButtonType type { get; set; }
+        private Texture2D button0 { get; set; }
+        private event EventHandler buttonPressed;
+
+        public event EventHandler ButtonPressed
+        {
+            get
+            {
+                switch (type)
+                {
+                    case ButtonType.Rectangle:
+                        return new Vector2(Collision.Location.X, Collision.Location.Y);
+                    default:
+                        return Vector2.Zero;
+                }
+            }
+        }
+
+        public Texture2D Texture
+        {
+            get { return button0; }
+        }
+
+        private int bNum;
+
+        public int ButtonNum
+        {
+            get { return bNum; }
+        }
+
+        private MouseState mouseState;
+
+        /// <summary>
+        /// Backing store for Collision.
+        /// </summary>
+        private Rectangle collision;
+
+        /// <summary>
+        /// Rectangle structure.
+        /// </summary>
+        public Rectangle Collison
+        {
+            get { return collision; }
+            set { collision = value; }
+        }
     }
 }
