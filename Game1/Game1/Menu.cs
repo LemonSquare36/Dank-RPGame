91a9fb32160b754fa34356e1e0ac8390380361d3
mamaher <mamaher@PRG23652>
2016-10-06 10:10:02 -0400

    changed button

--- Game1/Game1/Menu.cs
+++ Game1/Game1/Menu.cs
@@ -14,32 +14,8 @@ using System.Diagnostics;
 
 namespace RPGame
 {
-    public class Menu : Button
+    public class Menu
     {
 
-        public void Initialize()
-        {
-            int x = Window.ClientBounds.Width / 2 - buttonWidth / 2;
-            int y = Window.ClientBounds.Height / 2 -
-                numberOfButtons / 2 * buttonHeight -
-                (numberOfButtons % 2) * buttonHeight / 2;
-            for (int i = 0; i < numberOfButtons; i++)
-            {
-                button_state[i] = BState.UP;
-                button_color[i] = Color.White;
-                button_timer[i] = 0.0;
-                button_rectangle[i] = new Rectangle(x, y, buttonWidth, buttonHeight);
-                y += buttonHeight;
-            }
-            IsMouseVisible = true;
-            background_color = Color.CornflowerBlue;
-        }
-        protected override void LoadContent()
-        {
-            button_texture[Play] =
-                Content.Load<Texture2D>(@"buttons/Play");
-            button_texture[Play_Hover] =
-                Content.Load<Texture2D>(@"buttons/Ply_Hover");
-        }
     }
 }
