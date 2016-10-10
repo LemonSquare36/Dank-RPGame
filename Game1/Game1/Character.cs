a99ed70cdeffa4426e6d8f77046a6dfb68b27d5c
IsaacAllen <iallen@students.inghamisd.org>
2016-10-10 08:37:02 -0400

    Updated the Charater class

--- Game1/Game1/Character.cs
+++ Game1/Game1/Character.cs
@@ -14,17 +14,36 @@ using System.Diagnostics;
 
 namespace RPGame
 {
-   /*class Character : Polygons
+    class Character
     {
-        protected int health;
-        protected int ability;
-        protected int attack;
-        protected int level;
-        protected int xp;
-    }
+        KeyboardState charKeyBoardState;
 
-    private Character(int health, int ability, int attack, int level, int xp)
-    {
-        return (Player1);
-    }*/
+        protected int health = 20;
+        protected int ability = 10;
+        protected int attack = 10;
+        protected int level = 1;
+        protected int xp = 0;
+
+        protected Random rand;
+
+        private void LevelUp()
+        {
+            level++;
+            health = health + rand.Next(1, 7);
+            ability = ability + rand.Next(1, 7);
+            attack = attack + rand.Next(1, 7);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (charKeyBoardState.IsKeyDown(Keys.L))
+            {
+                LevelUp();
+                Debug.WriteLine(health);
+                Debug.WriteLine(ability);
+                Debug.WriteLine(attack);
+                Debug.WriteLine(level);
+            }
+        }
+    }
 }
