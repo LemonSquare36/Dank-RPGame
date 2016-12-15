5dd0d2101ce74265b7e1b067de002daba11a5f71
IsaacAllen <iallen@students.inghamisd.org>
2016-12-15 08:43:06 -0500

    Syncing

---
+++ Game1/Game1/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+using System.Diagnostics;
+using System.IO;
+
+namespace RPGame
+{
+    class Enemy : Entity
+    {
+        public Enemy(List<Vector2> numbers):base(numbers) { }
+
+
+    }
+}
