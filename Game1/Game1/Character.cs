0127de97e0a361e22ab75414e8d51aa78cfa94f5
iallen <iallen@PRG22737>
2017-01-23 08:40:39 -0500

    Grabbing menu code from michael

--- Game1/Game1/Character.cs
+++ Game1/Game1/Character.cs
@@ -37,7 +37,7 @@ namespace RPGame
         /// <param name="area"></param>
         public void LoadCharacter(string area)
         {
-            texture = Main.GameContent.Load<Texture2D>("Sprites/Janitor");
+            texture = Main.GameContent.Load<Texture2D>("Sprites/WalkCycle");
 
             if (area == "HabitationJanitorDoor")
                 Placement = new Vector2(400, 100);
