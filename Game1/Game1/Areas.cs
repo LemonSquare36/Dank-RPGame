ed56e61b17b5528c85c7d2ab4186d0a21aec882d
iallen <iallen@PRG22737>
2017-01-30 09:07:11 -0500

    isBullshit  =  true;

--- Game1/Game1/Areas.cs
+++ Game1/Game1/Areas.cs
@@ -19,7 +19,7 @@ namespace RPGame
 
     class Areas : Screen
     {
-
+        public bool isBullshit = true;
         /*! This Polygon based code is used by polygons in the Areas and Buttons in the Menus. !*/
 
         //Creates the Shapes of Polygon Clas
@@ -32,8 +32,6 @@ namespace RPGame
         }
         //Creates the Character like CreateShape
         protected Character CreateChar(string shapeName)
-
-
         {
 
             List<Vector2> NewList = (List<Vector2>)shapeVerts[shapeName];
