199dace1d712e367909afb1bb043c33833e39148
iallen <iallen@PRG22737>
2017-02-03 08:42:02 -0500

    Syncying up

---
+++ Game1/Game1/Goop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace RPGame
+{
+    class Goop : Entity
+
+    {
+        public Goop(List<Vector2> numbers) : base(numbers) { }
+       
+    }
+}
